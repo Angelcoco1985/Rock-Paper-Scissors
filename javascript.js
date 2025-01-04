e59e1623940ffe69c2ed0d6e6cc5c27ae542c1b9
Angel C <[170134244+Angelcoco1985@users.noreply.github.com]>
2025-01-04 15:10:33 -0800

    Create function that chooses rock paper scissors

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+let randomNum = Math.random() * 1;
+function getComputerChoice() {
+    // let randomNum = Math.random * 1;
+    if (randomNum >= 0.66) {
+        return "Rock";
+    }
+    else if (randomNum >= 0.33) {
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
+    
+}
+
+console.log(getComputerChoice());
