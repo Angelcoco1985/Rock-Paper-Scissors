c431e5585517b9c1dd58e60e0a37b17114841a6d
Angel C <[170134244+Angelcoco1985@users.noreply.github.com]>
2025-01-05 01:39:39 -0800

    Write logic to play a single round with the playround function

--- javascript.js
+++ javascript.js
@@ -16,16 +16,63 @@ function getComputerChoice() {
     
 }
 
-console.log(getComputerChoice());
-
 function getHumanChoice() {
-    let userInput = prompt("Choose a number 1: Rock, 2: Paper, 3: Scissors");
-    if (userInput === "1" || userInput === "3" || userInput === "3" ) {
-        return console.log(userInput);
+    let userInput = prompt("Type an option between: Rock, Paper, Scissors");
+    if (userInput.toUpperCase = "ROCK") {
+        return "Rock";
+    }
+    else if (userInput.toUpperCase = "PAPER") {
+        return "Paper";
+    }
+    else if (userInput.toUpperCase = "SCISSORS") {
+        return "Scissors";
     }
     else {
-        return console.log("Not a correct value");
+        return "Incorrect value";
     }
 }
 
-getHumanChoice();
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return "its a draw";
+    }
+
+    else if (humanChoice == "Rock" && computerChoice == "Paper") {
+        computerScore++;
+        return "You lose paper beats rock";
+    }
+
+    else if (humanChoice == "Rock" && computerChoice == "Scissors") {
+        humanScore++;
+        return "You win rock beats scissors";
+    }
+
+    else if (humanChoice == "Paper" && computerChoice == "Rock") {
+        humanScore++;
+        return "You win paper beats rock";
+    }
+
+    else if (humanChoice == "Paper" && computerChoice == "Scissors") {
+        computerScore++;
+        return "You lose Scissors beats paper";
+    }
+
+    else if (humanChoice == "Scissors" && computerChoice == "Paper") {
+        humanScore++;
+        return "You Win Scissors beats paper";
+    }
+
+    else if (humanChoice == "Scissors" && computerChoice == "Rock") {
+        computerScore++;
+        return "You lose Rock beats scissors";
+    }
+
+    else {
+        return "invalid input";
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
