5df6e48bc9b43883322284a7ad5ba1853320eee9
Angel C <[170134244+Angelcoco1985@users.noreply.github.com]>
2025-01-05 18:56:13 -0800

    implemented playGame which playsRound 5 times and used a for loop along with made score counters var to make them global

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
-// initalize variables to keep score
-let humanScore = 0;
-let computerScore = 0;
+// initalize variables globally to keep score
+var humanScore = 0;
+var computerScore = 0;
 
 function getComputerChoice() {
-    let randomNum = Math.random() * 1;
+    let randomNum = Math.random();
     if (randomNum >= 0.66) {
         return "Rock";
     }
@@ -18,13 +18,13 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let userInput = prompt("Type an option between: Rock, Paper, Scissors");
-    if (userInput.toUpperCase = "ROCK") {
+    if (userInput.toUpperCase() === "ROCK") {
         return "Rock";
     }
-    else if (userInput.toUpperCase = "PAPER") {
+    else if (userInput.toUpperCase() === "PAPER") {
         return "Paper";
     }
-    else if (userInput.toUpperCase = "SCISSORS") {
+    else if (userInput.toUpperCase() === "SCISSORS") {
         return "Scissors";
     }
     else {
@@ -32,47 +32,67 @@ function getHumanChoice() {
     }
 }
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        return "its a draw";
+function playGame() {
+
+    function playRound(humanChoice, computerChoice) {
+
+        if (humanChoice === computerChoice) {
+            return "its a draw";
+        }
+    
+        else if (humanChoice === "Rock" && computerChoice === "Paper") {
+            computerScore++;
+            return "You lose paper beats rock";
+        }
+    
+        else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+            humanScore++;
+            return "You win rock beats scissors";
+        }
+    
+        else if (humanChoice === "Paper" && computerChoice === "Rock") {
+            humanScore++;
+            return "You win paper beats rock";
+        }
+    
+        else if (humanChoice === "Paper" && computerChoice === "Scissors") {
+            computerScore++;
+            return "You lose Scissors beats paper";
+        }
+    
+        else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+            humanScore++;
+            return "You Win Scissors beats paper";
+        }
+    
+        else if (humanChoice === "Scissors" && computerChoice === "Rock") {
+            computerScore++;
+            return "You lose Rock beats scissors";
+        }
+    
+        else {
+            return "invalid input";
+        }
     }
 
-    else if (humanChoice == "Rock" && computerChoice == "Paper") {
-        computerScore++;
-        return "You lose paper beats rock";
+    for (let i=0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
     }
 
-    else if (humanChoice == "Rock" && computerChoice == "Scissors") {
-        humanScore++;
-        return "You win rock beats scissors";
+    if (humanScore > computerScore) {
+        console.log("You Win");
     }
 
-    else if (humanChoice == "Paper" && computerChoice == "Rock") {
-        humanScore++;
-        return "You win paper beats rock";
-    }
-
-    else if (humanChoice == "Paper" && computerChoice == "Scissors") {
-        computerScore++;
-        return "You lose Scissors beats paper";
-    }
-
-    else if (humanChoice == "Scissors" && computerChoice == "Paper") {
-        humanScore++;
-        return "You Win Scissors beats paper";
-    }
-
-    else if (humanChoice == "Scissors" && computerChoice == "Rock") {
-        computerScore++;
-        return "You lose Rock beats scissors";
+    else if (humanScore < computerScore) {
+        console.log("You lose");
     }
 
     else {
-        return "invalid input";
+        console.log("Its a draw");
     }
+
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();
